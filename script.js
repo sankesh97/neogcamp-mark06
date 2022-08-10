187bd2f7237db28bb7abd2d209db523538bd026a
sankesh97 <ramojitinorange@gmail.com>
2022-08-10 14:37:06 +0530

    Added Stylesheet, JS files and added the input and out content in HTML file.

---
+++ script.js
@@ -0,0 +1,16 @@
+let inputText = document.querySelector("#input");
+let outputText = document.querySelector("#output").value;
+
+let translateBinion = () => {
+  console.log(inputText.value);
+  fetch(
+    "https://api.funtranslations.com/translate/minion.json?text=" +
+      inputText.value
+  )
+    .then((response) => response.json())
+    .then((data) => (outputText.innerText = data.contents.translated))
+    .catch((error) => {
+      console.log(error);
+      alert("Something went wrong! Please try after some time.");
+    });
+};
